be3a18a9e4c5bccba97c1af027b52c1c183cb9b7
jordasche <80426378+jordasche@users.noreply.github.com>
2021-08-25 22:55:23 -0400

    - Added axios - Made MediaRows dynamic with moviedb api data - Deleted home.js page

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { shuffleArray } from "../utilities";
+
 const MediaRow = (props) => {
+   const [loadingData, setLoadingData] = useState(true);
+   const [movies, setMovies] = useState([]);
+
+   useEffect(() => {
+      axios
+         .get(
+            `https://api.themoviedb.org/3/${props.endpoint}&api_key=8b4d9144732c62a3656d7c80c4753668&language=en-US`
+         )
+         .then(function (response) {
+            setMovies(shuffleArray(response.data.results));
+            setLoadingData(false);
+            console.log("Success Response For " + props.title);
+            console.log(response);
+         })
+         .then(function (error) {
+            console.log(error);
+         });
+   }, []);
+
    const loopComp = (comp, digit) => {
       let thumbnails = [];
       for (let index = 1; index <= digit; index++) {
@@ -6,22 +29,38 @@ const MediaRow = (props) => {
       }
       return thumbnails;
    };
+   const showThumnails = () => {
+      return loadingData
+         ? loopComp(<Skeleton />, 10)
+         : movies.map((movie) => {
+              return <Thumbnail movieData={movie} />;
+           });
+   };
    return (
       <div className={`media-row ${props.type}`}>
          <h3 className="media-row__title">{props.title}</h3>
-         <div className="media-row__thumbnails">
-            {loopComp(
-               <div className="media-row__thumbnail">
-                  <img
-                     src="https://i.etsystatic.com/13367669/r/il/db21fd/2198543930/il_570xN.2198543930_4qne.jpg"
-                     alt=""
-                  />
-                  <div className="media-row__top-layer">
-                     <i className="fas fa-play"></i>
-                  </div>
-               </div>,
-               10
-            )}
+         <div className="media-row__thumbnails">{showThumnails()}</div>
+      </div>
+   );
+};
+
+const Skeleton = () => {
+   return (
+      <div className="media-row__thumbnail-skeleton">
+         <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+   );
+};
+
+const Thumbnail = (props) => {
+   return (
+      <div className="media-row__thumbnail">
+         <img
+            src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path}`}
+            alt=""
+         />
+         <div className="media-row__top-layer">
+            <i className="fas fa-play"></i>
          </div>
       </div>
    );
--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,36 @@ export default function Home() {
       <div>
          <MainLayout>
             <FeaturedMedia></FeaturedMedia>
-            <MediaRow title="Movies" type="large-v"></MediaRow>
-            <MediaRow title="Series" type="small-v"></MediaRow>
-            <MediaRow title="Action" type="small-h"></MediaRow>
-            <MediaRow title="Horror" type="large-h"></MediaRow>
-            <MediaRow title="Sci-fi" type="large-v"></MediaRow>
+            <MediaRow
+               title="Movies"
+               type="large-v"
+               endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
+            ></MediaRow>
+            <MediaRow
+               title="Series"
+               type="small-v"
+               endpoint="discover/tv?sort_by=popularity.desc&primary_release_year=2021"
+            ></MediaRow>
+            <MediaRow
+               title="Action"
+               type="small-h"
+               endpoint="discover/movie?with_genres=28&primary_release_year=2021"
+            ></MediaRow>
+            <MediaRow
+               title="Horror"
+               type="large-h"
+               endpoint="discover/movie?with_genres=27&primary_release_year=2021"
+            ></MediaRow>
+            <MediaRow
+               title="Animations"
+               type="large-h"
+               endpoint="discover/movie?with_genres=16&primary_release_year=2021"
+            ></MediaRow>
+            <MediaRow
+               title="Sci-fi"
+               type="large-v"
+               endpoint="discover/movie?with_genres=878&primary_release_year=2021"
+            ></MediaRow>
          </MainLayout>
       </div>
    );
